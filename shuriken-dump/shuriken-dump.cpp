d06a4a1ed19457f382dec34cda43bce681c3e722
Fare9 <eduardo.blazquez@uc3m.es>
2024-01-30 23:22:59 +0100

    Created all dalvik instructions for the disassembler.

--- shuriken-dump/shuriken-dump.cpp
+++ shuriken-dump/shuriken-dump.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 #include <shuriken/parser/shuriken_parsers.h>
 #include <shuriken/parser/Dex/parser.h>
-#include <shuriken/parser/Dex/dvm_types.h>
+#include <shuriken/common/Dex/dvm_types.h>
 #include <fmt/core.h>
 
 
