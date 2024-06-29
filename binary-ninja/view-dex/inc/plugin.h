f6471c099da43723e3ca09845fac2a06e8dbe482
robert-yates <robert-yates@users.noreply.github.com>
2024-06-29 04:17:05 +0200

    dexview: setup #2

--- binary-ninja/view-dex/inc/plugin.h
+++ binary-ninja/view-dex/inc/plugin.h
@@ -3,29 +3,29 @@
 #include "binaryninjaapi.h"
 #include <cstdint>
 
+
 namespace BinaryNinja {
 
 class DEXViewType : public BinaryViewType {
-    Ref<Logger> m_logger;
-
   public:
     DEXViewType();
     virtual Ref<BinaryView> Create(BinaryView* data) override;
     virtual Ref<BinaryView> Parse(BinaryView* data) override;
     virtual bool IsTypeValidForData(BinaryView* data) override;
     virtual Ref<Settings> GetLoadSettingsForData(BinaryView* data) override;
+
+  private:
+    Ref<Logger> m_logger;
 };
 void InitDEXViewType();
 
 class DEXView : public BinaryView {
   public:
     DEXView(BinaryView* data, bool parseOnly = false);
-
     virtual bool Init() override;
 
   protected:
     virtual uint64_t PerformGetEntryPoint() const override;
-
     virtual bool PerformIsExecutable() const override { return true; }
     virtual BNEndianness PerformGetDefaultEndianness() const override { return LittleEndian; }
     virtual bool PerformIsRelocatable() const override { return m_relocatable; }
@@ -35,8 +35,13 @@ class DEXView : public BinaryView {
     bool m_relocatable = false;
     bool m_parseOnly;
     bool m_backedByDatabase;
+    uint64_t m_imageBase = 0;
     Ref<Logger> m_logger;
     Ref<Architecture> m_arch;
+    Ref<Platform> m_platform;
+
+    void buildStructures();
+    void buildFunctions();
 };
 
 } // namespace BinaryNinja
--- binary-ninja/view-dex/src/dexview.cpp
+++ binary-ninja/view-dex/src/dexview.cpp
@@ -9,29 +9,83 @@ DEXView::DEXView(BinaryView* data, bool parseOnly) : BinaryView("DEX", data->Get
 }
 
 bool DEXView::Init() {
+    m_logger->LogError("DEXView::Init()");
     Ref<Settings> settings = GetLoadSettings(GetTypeName());
     Ref<Settings> viewSettings = Settings::Instance();
 
     const uint64_t alignment = 0x1000;
-    uint64_t baseAddress = 0;
     const uint64_t rawFileOffset = 0;
     const uint64_t dexCodeSegmentSize = (GetParentView()->GetLength() + alignment - 1) & ~(alignment - 1);
-    const uint64_t fieldDataSegmentAddress = baseAddress + dexCodeSegmentSize;
+    const uint64_t fieldDataSegmentAddress = m_imageBase + dexCodeSegmentSize;
     const uint64_t fieldDataSegmentSize = 0x1000;
 
     // TODO: create/use arch-dex
-    Ref<Architecture> arch = Architecture::GetByName("x86");
-    SetDefaultArchitecture(arch);
-    Ref<Platform> platform = arch->GetStandalonePlatform();
-    SetDefaultPlatform(platform);
+    m_arch = Architecture::GetByName("x86");
+    m_platform = m_arch->GetStandalonePlatform();
+    SetDefaultArchitecture(m_arch);
+    SetDefaultPlatform(m_platform);
 
-    AddAutoSegment(baseAddress, GetParentView()->GetLength(), rawFileOffset, GetParentView()->GetLength(), SegmentReadable);
+    AddAutoSegment(m_imageBase, GetParentView()->GetLength(), rawFileOffset, GetParentView()->GetLength(), SegmentReadable);
     AddAutoSegment(fieldDataSegmentAddress, fieldDataSegmentSize, 0, 0x100, SegmentWritable);
-
     AddAutoSection("code", 0, dexCodeSegmentSize, ReadOnlyDataSectionSemantics);
     AddAutoSection("fields", fieldDataSegmentAddress, fieldDataSegmentSize, ReadWriteDataSectionSemantics);
 
+    buildStructures();
+    buildFunctions();
+
     return true;
+}
+
+void DEXView::buildFunctions() {
+    
+/*
+    std::unique_ptr<DataReader> shurikenReader = std::make_unique<DataReader>(GetParentView());
+    shurikenapi::parse_dex(shurikenReader.get());
+*/
+
+    /* Sample Add Function
+    auto cc = m_platform->GetDefaultCallingConvention();
+    auto functionPointer = Type::PointerType(m_platform->GetArchitecture(), Type::FunctionType(Type::VoidType(), cc, {}));
+
+    auto exampleFunctionType = Type::FunctionType(Type::VoidType(), cc, {FunctionParameter("", functionPointer)});
+    auto funcType = Type::PointerType(m_platform->GetArchitecture(), exampleFunctionType);
+    DefineAutoSymbolAndVariableOrFunction(GetDefaultPlatform(), new Symbol(FunctionSymbol, "func01", 0x200, NoBinding), funcType);
+    */
+}
+
+void DEXView::buildStructures() {
+
+    StructureBuilder dexHeaderBuilder;
+    dexHeaderBuilder.AddMember(Type::ArrayType(Type::IntegerType(1, true), 8), "magic");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "checksum");
+    dexHeaderBuilder.AddMember(Type::ArrayType(Type::IntegerType(1, false), 20), "signature");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "file_size");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "header_size");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "endian_tag");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "link_size");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "link_off");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "map_off");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "string_ids_size");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "string_ids_off");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "type_ids_size");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "type_ids_off");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "proto_ids_size");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "proto_ids_off");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "field_ids_size");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "field_ids_off");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "method_ids_size");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "method_ids_off");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "class_defs_size");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "class_defs_off");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "data_size");
+    dexHeaderBuilder.AddMember(Type::IntegerType(4, false), "data_off");
+    Ref<Structure> dexHeaderStruct = dexHeaderBuilder.Finalize();
+
+    Ref<Type> dexHeaderType = Type::StructureType(dexHeaderStruct);
+    QualifiedName dexHeaderName = std::string("DEX_Header");
+    QualifiedName dexHeaderTypeName = DefineType(Type::GenerateAutoTypeId("dex", dexHeaderName), dexHeaderName, dexHeaderType);
+    DefineDataVariable(m_imageBase, Type::NamedType(this, dexHeaderTypeName));
+    DefineAutoSymbol(new Symbol(DataSymbol, "__dex_header", m_imageBase, NoBinding));
 }
 
 uint64_t DEXView::PerformGetEntryPoint() const { return 0; }
