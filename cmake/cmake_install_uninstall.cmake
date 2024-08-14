6ee096e7a06ae25cd6497df5ed5e8640905795e5
Jasmine Tang <tanghocle456@gmail.com>
2024-08-14 09:30:01 -0700

    Cmake improve (#50)
    * CI/CD: Fix uninstall directions
    
    * BUILD: Factor out install and uninstall to cmake/
    
    * DOCS: Update install and uninstall diretions
    
    * BUILD: Get link path from main CMakeLists-> cmake
    
    * Add a step to check if all uninstallation works
    
    * BUILD: Fix build step in CONTRIBUTING.md

---
+++ cmake/cmake_install_uninstall.cmake
@@ -0,0 +1,44 @@
+
+#------------------------------------------------------------- Installation Flags
+# Default paths for installation
+# Set CMake MacOS runtime path to find .so files on mac
+set(CMAKE_MACOSX_RPATH 1)
+# Append runtime path to find .so files on unix, needed before install
+list( APPEND CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib" )
+
+# Check for operating system and set install paths accordingly
+# Conditional installation paths for different platforms
+if(APPLE)
+    find_program(HOMEBREW_FOUND brew)
+    if(HOMEBREW_FOUND)
+        execute_process(COMMAND brew --prefix
+                OUTPUT_VARIABLE HOMEBREW_PREFIX
+                 OUTPUT_STRIP_TRAILING_WHITESPACE)
+        set(include_install_path "${HOMEBREW_PREFIX}/include")
+        set(library_install_path "${HOMEBREW_PREFIX}/lib")
+        set(CMAKE_INSTALL_PREFIX "${HOMEBREW_PREFIX}")
+    endif()
+elseif(UNIX AND NOT APPLE) # Explicitly differentiate UNIX from APPLE
+    # Linux specific paths (already set as default)
+    if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+        set(CMAKE_INSTALL_PREFIX "/usr/local" CACHE PATH "Default installation directory" FORCE)
+    endif()
+    # Define default include and library paths based on CMAKE_INSTALL_PREFIX
+    set(include_install_path "${CMAKE_INSTALL_PREFIX}/include") # Default path
+    set(library_install_path "${CMAKE_INSTALL_PREFIX}/lib") # Default path
+endif()
+
+
+#------------------------------------------------------ Uninstallation Flags
+# Define the uninstall target
+if(NOT TARGET uninstall)
+    configure_file(
+        "${CMAKE_SOURCE_DIR}/cmake/cmake_install_uninstall.cmake.in"
+        "${CMAKE_BINARY_DIR}/cmake/cmake_install_uninstall.cmake"
+        IMMEDIATE @ONLY
+    )
+
+    add_custom_target(uninstall
+        COMMAND ${CMAKE_COMMAND} -P ${CMAKE_BINARY_DIR}/cmake/cmake_install_uninstall.cmake
+    )
+endif()
