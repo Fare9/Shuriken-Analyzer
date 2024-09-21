f9e46963873e2ac6bcf0ae2b0696b60c71313bc8
Fare9 <eduardo.blazquez@uc3m.es>
2024-09-21 21:02:35 +0200

    Started APK support in Core API

--- shuriken/include/shuriken/api/C/shuriken_core_data.h
+++ shuriken/include/shuriken/api/C/shuriken_core_data.h
@@ -28,7 +28,10 @@
 
 extern "C" {
 
+/// @brief opaque pointers user will use for calling the different methods
 typedef void *hDexContext;
+
+typedef void *hApkContext;
 
 ///--------------------------- Parser Data ---------------------------
 
@@ -356,6 +359,8 @@ typedef struct hdvmfieldanalysis_t_ {
     hdvm_class_method_idx_t *xrefwrite;
 } hdvmfieldanalysis_t;
 
+/// @brief Structure to keep information about the string analysis
+/// [UNUSED FOR NOW]
 typedef struct hdvmstringanalysis_t_ {
     /// @brief value of that string
     const char *value;
@@ -365,6 +370,7 @@ typedef struct hdvmstringanalysis_t_ {
     hdvm_class_method_idx_t *xreffrom;
 } hdvmstringanalysis_t;
 
+/// @brief Structure to keep information about the method analysis
 typedef struct hdvmmethodanalysis_t_ {
     /// @brief name of the method
     const char *name;
@@ -410,6 +416,7 @@ typedef struct hdvmmethodanalysis_t_ {
     const char *method_string;
 } hdvmmethodanalysis_t;
 
+/// @brief Structure to keep information about the class analysis
 typedef struct hdvmclassanalysis_t_ {
     /// @brief is external class?
     int is_external;
---
+++ shuriken/tests/apk/apk-core-api-test.cpp
@@ -0,0 +1,41 @@
+//--------------------------------------------------------------------*- C++ -*-
+// Shuriken-Analyzer: library for bytecode analysis.
+// @author Farenain <kunai.static.analysis@gmail.com>
+//
+// @file core-api-test.c
+// @brief Test for the Core API in C of the project
+
+#include "shuriken/api/C/shuriken_core.h"
+#include "dex-files-folder.inc"
+
+#include <iostream>
+
+
+const char *file = DEX_FILES_FOLDER
+        "test_zip.apk";
+
+int main() {
+    hApkContext apk_context = parse_apk(file, 1);
+
+    int number_of_dex_files = get_number_of_dex_files(apk_context);
+
+    std::cout << "Number of dex files: " << number_of_dex_files << '\n';
+
+    for (int i = 0; i < number_of_dex_files; i++) {
+        const char * dex_file = get_dex_file_by_index(apk_context, i);
+
+        int number_of_classes = get_number_of_classes_for_dex_file(apk_context, dex_file);
+
+        std::cout << "Dex file: " << dex_file
+                  << ", number of classes: " << number_of_classes
+                  << '\n';
+
+        for (int j = 0; j < number_of_classes; j++) {
+            hdvmclass_t * clazz = get_hdvmclass_from_dex_by_index(apk_context, dex_file, j);
+
+            std::cout << "Class name: " << clazz->class_name << '\n';
+        }
+    }
+
+    destroy_apk(apk_context);
+}
